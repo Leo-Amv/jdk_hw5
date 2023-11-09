2e21957f92be17c4da9733685180b3809c3684f2
Leo <leonidamvrosov@yandex.ru>
2023-11-09 19:12:47 +0500

    hw completed

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -1,7 +1,26 @@
 package ru.gb;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        LogHandler logHandler = new LogHandler("./Chat_Log");
+
+        ChatWindow chat1 = new ChatWindow("user_1", logHandler); //поток чата 1
+        ChatWindow chat2 = new ChatWindow("user_2", logHandler); //потом чата 2
+        ChatWindow chat3 = new ChatWindow("user_3", logHandler); //поток чата 3
+
+
+        synchronized(logHandler){
+            while (true){
+                if (logHandler.dataChanged()){
+                    chat1.initWindow();
+                    chat2.initWindow();
+                    chat3.initWindow();
+                    logHandler.setDataChanged(false);
+                }
+            }
+        }
     }
 }
